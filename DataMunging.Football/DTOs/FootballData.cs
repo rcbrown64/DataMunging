4eca1a5d2574834a4b669fedd115cc5e4a11431d
Robert Brown <brownrc@live.com>
2022-12-16 22:53:17 -0500

    Remove unused items

--- DataMunging.Football/DTOs/FootballData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataMunging.Football.DTOs
-{
-    public class FootballData
-    {
-        public string? Team { get; set; }
-        public int For { get; set; }
-        public int Against { get; set; }
-    }
-}
