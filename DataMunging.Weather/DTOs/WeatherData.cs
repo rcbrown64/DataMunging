51623cb5e98981c05c38b0edb6f5eba61faf0bfd
Robert Brown <brownrc@live.com>
2022-12-16 21:17:00 -0500

    Implement weather comparison

---
+++ DataMunging.Weather/DTOs/WeatherData.cs
@@ -0,0 +1,9 @@
+﻿namespace DataMunging.Weather.DTOs
+{
+    public class WeatherData
+    {
+        public int Dy { get; set; }
+        public double MxT { get; set; }
+        public double MnT { get; set; }
+    }
+}
--- DataMunging/Program.cs
+++ DataMunging/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataMunging.Common.Utilities.FileSystem;
+using DataMunging.Common.Utilities.Parsing;
+using DataMunging.Weather.DTOs;
+
+var lines = FileUtils.GetFileLines(@"Assets\weather.dat");
+var minSpreadData = 
+    (from line in lines
+     let weatherData = FlatFileParser.Parse<WeatherData>(line)
+     where weatherData.Dy != 0
+     orderby weatherData.MxT - weatherData.MnT 
+     select weatherData).First();
+
+Console.WriteLine($"Smallest spread - Day: {minSpreadData.Dy}, Spread width: {minSpreadData.MxT - minSpreadData.MnT}.");
